51b499f6b51636b09df7b8b3d3f342e089523836
Aitor Oses <aitor.oses@gmail.com>
2014-04-09 10:09:11 +0200

    Meteor rendering template on famous

--- client/famous.js
+++ client/famous.js
@@ -9,16 +9,46 @@ if (Meteor.isClient) {
 
 			mainCtx = Engine.createContext();
 
-			// Create a surface using a Meteor template
-			var surface = new Surface({
+			// var r = UI.render(Template.test);
+			// html = r.dom.members[1];
+			// debugger
+
+			// // Create a surface using a Meteor template
+			// var surface = new Surface({
+			// 	size: [100, 100],
+			// 	classes: ["test-surface"],
+			// 	content: r.dom.getNodes()
+			// });
+
+			function createSurface(s) {
+				var r;
+				if (s.data) {
+					r = UI.renderWithData(s.content, s.data);
+				} else {
+					r = UI.render(s.content);
+				}
+				htmlNodes = r.dom.getNodes();
+				var node;
+				if (htmlNodes.length == 3) {
+					node = r.dom.members[1];
+				} else {
+					var node = document.createElement("div");
+					for (var i = 1; i < htmlNodes.length; i++) {
+						node.appendChild(htmlNodes[i]);
+					}
+				}
+				return new Surface({
+					size: s.size,
+					classes: s.classes,
+					content: node
+				});
+			}
+
+			var surface = createSurface({
 				size: [100, 100],
 				classes: ["test-surface"],
-			});
-
-			var r = UI.render(Template.test);
-			html = r.dom.members[1];
-			Meteor.defer(function(){
-				$(".test-surface").append(r.dom.members[1]);
+				content: Template.test,
+				data: "Template data"
 			});
 
 			mainCtx.add(surface);
